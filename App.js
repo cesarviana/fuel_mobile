f99f346970c53aa6470784cef1bfebce7902445a
Cesar Viana <cesarviana@edu.univali.br>
2018-12-16 18:13:43 -0200

    Button list

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Button, TextInput } from 'react-native';
 import HelloWorld from './components/HelloWorld'
 
 export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.values = [50, 100]
+    this.price = '4,05'
+  }
+  selectValue(x) {
+    alert(x)
+  }
   render() {
     return (
       <View style={styles.container}>
-        <HelloWorld name='Cesar'/>
+        { 
+          this.values.map(value=>
+            <Button key={value} 
+                    title={value.toString()} 
+                    onPress={ this.selectValue.bind(this,value) }
+                    accessibilityLabel={`Seleciona valor ${value} em para abastecer.`}
+                    />
+          ) 
+        }
+        <HelloWorld name='Teste'/>
       </View>
     );
   }
